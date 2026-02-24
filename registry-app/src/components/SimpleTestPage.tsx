dc4fb8ec905722313e3b3a96380e4b5e27571421
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-24 23:06:39 +0300

    elasticsearch

--- registry-app/src/components/SimpleTestPage.tsx
+++ registry-app/src/components/SimpleTestPage.tsx
@@ -1,8 +1,27 @@
+const ES = "http://localhost:9200";
+const INDEX = "simple_test";
+
+async function createIndex() {
+  await fetch(`${ES}/${INDEX}`, { method: "PUT" });
+}
+
+async function generateTestData() {
+  createIndex();
+  for (let i = 0; i < 10; i++) {
+    const doc = { id: i, name: `item ${i}` };
+    await fetch(`${ES}/${INDEX}/_doc`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(doc),
+    });
+  }
+}
+
 function SimpleTestPage() {
   return (
     <div style={{ padding: 16 }}>
-      <button onClick={() => {}}>Generate</button>
-      <button onClick={() => {}}>Query</button>
+      <button onClick={generateTestData}>Generate test data</button>
+      {/* <button onClick={generate}>Generate</button> */}
     </div>
   );
 }
