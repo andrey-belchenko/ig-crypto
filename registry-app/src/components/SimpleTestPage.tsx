cd2e0dd475d00c856179e5d5494fae45a2056821
Andrey Belchenko <andrey.v.belchenko@gmail.com>
2026-02-24 23:09:47 +0300

    elasticsearch

--- registry-app/src/components/SimpleTestPage.tsx
+++ registry-app/src/components/SimpleTestPage.tsx
@@ -1,19 +1,18 @@
-const ES = "http://localhost:9200";
+import axios from "axios";
+
+const ES = import.meta.env.DEV ? "/es" : "http://localhost:9200";
 const INDEX = "simple_test";
 
-async function createIndex() {
-  await fetch(`${ES}/${INDEX}`, { method: "PUT" });
+async function resetIndex() {
+  await axios.delete(`${ES}/${INDEX}`).catch(() => {});
+  await axios.put(`${ES}/${INDEX}`);
 }
 
 async function generateTestData() {
-  createIndex();
+  await resetIndex();
   for (let i = 0; i < 10; i++) {
     const doc = { id: i, name: `item ${i}` };
-    await fetch(`${ES}/${INDEX}/_doc`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(doc),
-    });
+    await axios.post(`${ES}/${INDEX}/_doc`, doc);
   }
 }
 
@@ -21,7 +20,7 @@ function SimpleTestPage() {
   return (
     <div style={{ padding: 16 }}>
       <button onClick={generateTestData}>Generate test data</button>
-      {/* <button onClick={generate}>Generate</button> */}
+      <button onClick={generateTestData}>Query test data</button>
     </div>
   );
 }
